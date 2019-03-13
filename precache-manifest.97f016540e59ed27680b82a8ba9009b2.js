9808e6eba2725ea6b588a0dfc1c51fd6dcb7fcbf
Pedro <pedrozo_96@hotmail.com>
2019-03-12 23:05:55 -0300

    Updates

---
+++ precache-manifest.97f016540e59ed27680b82a8ba9009b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/animelist/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d1236b6fefe502319613",
+    "url": "/animelist/static/css/main.9ea12de0.chunk.css"
+  },
+  {
+    "revision": "ad8e47aeb158979d2448",
+    "url": "/animelist/static/js/runtime~main.f37d6ac7.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/animelist/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8fd6eeba83f53f6f4988",
+    "url": "/animelist/static/js/2.25bfd1b7.chunk.js"
+  },
+  {
+    "revision": "d1236b6fefe502319613",
+    "url": "/animelist/static/js/main.1fb1bb90.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/animelist/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/animelist/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/animelist/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "8fd6eeba83f53f6f4988",
+    "url": "/animelist/static/css/2.9de4da68.chunk.css"
+  },
+  {
+    "revision": "806769f37291b8b1c0bda249e1798be0",
+    "url": "/animelist/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pr-sanchez/animelist/precache-manifest.6be1ca6632ea6595dce12df54d8acdb2.js"
+  "/animelist/precache-manifest.97f016540e59ed27680b82a8ba9009b2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pr-sanchez/animelist/index.html", {
+workbox.routing.registerNavigationRoute("/animelist/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
